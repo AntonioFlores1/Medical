b9421419d1d68c2830ed7f83b9c89ba10e6cff07
antonio <antonio@antonios-mbp.home>
2020-01-28 00:28:50 -0500

    updated colletionviewCell UI

--- MedicalApp/HumanSymptomsCollectionViewCell.swift
+++ MedicalApp/HumanSymptomsCollectionViewCell.swift
@@ -4,14 +4,51 @@
 //
 //  Created by antonio  on 1/10/20.
 //  Copyright © 2020 Pursuit. All rights reserved.
-//
+//  symptoms
 
 import UIKit
 
 class HumanSymptomsCollectionViewCell: UICollectionViewCell {
     
+    lazy var symptomsLabel: UILabel = {
+        var label = UILabel()
+        label.textColor = .black
+        label.setContentHuggingPriority(.fittingSizeLevel, for: .horizontal)
+        label.font = UIFont(name: "Didot", size: 16)
+        return label
+    }()
     
-    @IBOutlet weak var sympotomsInfo: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        symptomsConstraints()
+//        self.layer.cornerRadius = self.frame.size.height / 2
+
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+    
+    func symptomsConstraints() {
+        addSubview(symptomsLabel)
+        symptomsLabel.translatesAutoresizingMaskIntoConstraints = false
+        symptomsLabel.topAnchor.constraint(equalTo: contentView.topAnchor,
+                                           constant: 0).isActive = true
+        symptomsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor,
+                                              constant: 0).isActive = true
+        symptomsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        symptomsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 2).isActive = true
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        self.contentView.layer.cornerRadius = 90
+    }
     
 }
--- MedicalApp/View Controller/SymptomsViewController.swift
+++ MedicalApp/View Controller/SymptomsViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate {
+class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate,MICollectionViewBubbleLayoutDelegate {
+    
     
     @IBOutlet weak var symptomsSearchBar: UISearchBar!
     
@@ -18,27 +19,54 @@ class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDa
     
     private lazy var humansSymptomCollectionView: UICollectionView = {
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-        layout.itemSize = CGSize(width: 70, height: 70)
-        
-        let collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+//        layout.itemSize = CGSize(width: 70, height: 70)
+        layout.scrollDirection = .vertical
+//        layout.minimumInteritemSpacing = 30
+//        layout.minimumLineSpacing = 100
+//        layout.estimatedItemSize = CGSize(width: 1, height: 0)
+//        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        let collectionView = UICollectionView(frame: self.view.frame,
+                                              collectionViewLayout: layout)
         collectionView.delegate = self
-//        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.dataSource = self
+        collectionView.register(HumanSymptomsCollectionViewCell.self,
+                                forCellWithReuseIdentifier: "humanSymptoms")
+//        collectionView.scrollIndicatorInsets = .zero
+//        collectionView.verticalScrollIndicatorInsets = .zero
         collectionView.backgroundColor = .blue
-        
+        let bubbleLayout = MICollectionViewBubbleLayout()
+        bubbleLayout.minimumLineSpacing = 6.0
+        bubbleLayout.minimumInteritemSpacing = 6.0
+        bubbleLayout.delegate = self
+        collectionView.setCollectionViewLayout(bubbleLayout, animated: false)
         return collectionView
     }()
+    
+    private lazy var continueButton: UIButton = {
+    var button = UIButton()
+        button.setTitle("Continue", for: .normal)
+        button.addTarget(self, action: #selector(diagnosisButton), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    @objc func diagnosisButton(){
+        
+    }
     
     func CollectionViewConstraints(){
         view.addSubview(humansSymptomCollectionView)
         view.insertSubview(symptomsTableView, aboveSubview: humansSymptomCollectionView)
 //        view.didAddSubview(humansSymptomCollectionView)
         humansSymptomCollectionView.translatesAutoresizingMaskIntoConstraints = false
-        humansSymptomCollectionView.topAnchor.constraint(equalTo: symptomsSearchBar.bottomAnchor, constant: 0).isActive = true
+        humansSymptomCollectionView.topAnchor.constraint(equalTo: symptomsSearchBar.bottomAnchor, constant: 25).isActive = true
         humansSymptomCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         humansSymptomCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        humansSymptomCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        humansSymptomCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -140).isActive = true
     }
+    
+
     
     
     
@@ -83,8 +111,8 @@ class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDa
         symptomsTableView.dataSource = self
         symptomsTableView.delegate = self
         symptomsSearchBar.delegate = self
-//        humansSymptomCollectionView.delegate = self
-//        humansSymptomCollectionView.dataSource = self
+        humansSymptomCollectionView.delegate = self
+        humansSymptomCollectionView.dataSource = self
         
         self.symptomsTableView.isHidden = true
         
@@ -95,12 +123,26 @@ class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDa
 ////        view.addSubview(collection)
 //        humansSymptomCollectionView.delegate = self
 //        humansSymptomCollectionView.dataSource = self
-        
-        let collectionViewFlowLayout = humansSymptomCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
+//        let collectionViewFlowLayout = humansSymptomCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
         
 
         
     }
+    
+    func collectionView(_ collectionView: UICollectionView, itemSizeAt indexPath: NSIndexPath) -> CGSize {
+        let title = humansSymptoms[indexPath.row] as! NSString
+        var size = title.size(withAttributes: [NSAttributedString.Key.font: UIFont(name: "Didot", size: 15)!])
+        size.width = CGFloat(ceilf(Float(size.width + CGFloat(15 * 2))))
+        size.height = 24
+        
+        //...Checking if item width is greater than collection view width then set item width == collection view width.
+        if size.width > collectionView.frame.size.width {
+            size.width = collectionView.frame.size.width
+        }
+        
+        return size;
+    }
+    
     
     func searchSymptoms(_ keyword:String){
     SymptomsAPI.ListOfSymptoms(keyword: keyword) { (appError, symptom) in
@@ -186,8 +228,6 @@ class SymptomsViewController: UIViewController,UITableViewDelegate,UITableViewDa
                 return
         }
         symptomsTableView.isHidden = false
-//        symptomsTableView.alpha = 1
-        
         searchSymptoms(searchText.lowercased())
 
     }
@@ -206,28 +246,31 @@ extension SymptomsViewController:UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let collectionCell = humansSymptomCollectionView.dequeueReusableCell(withReuseIdentifier: "humanSymptoms", for: indexPath) as? HumanSymptomsCollectionViewCell else {
-            
             return UICollectionViewCell()
         }
             
          let info = humansSymptoms[indexPath.row]
         
-        collectionCell.sympotomsInfo.text = info.separate(every: 8, with: "/n")
+        collectionCell.backgroundColor = #colorLiteral(red: 0.7335347533, green: 0, blue: 1, alpha: 1)
+        collectionCell.symptomsLabel.text = info
+        collectionCell.contentView.layer.cornerRadius = 10.0
+        collectionCell.layer.cornerRadius = 10.0
         
-        print(info.separate(every: 8, with: "/n "))
-
-//        cell.symptoms.text = info
-//        collectionCell.sizeToFit()
         return collectionCell
     }
     
 }
 
 extension SymptomsViewController: UICollectionViewDelegate {
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//        return CGSize(width:view.bounds.width - 40, height: view.bounds.height)
+//    }
 
-
-        return CGSize(width:view.bounds.width - 40, height: view.bounds.height)
-    }
     
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let collectionWidth = collectionView.bounds.width
+//        return CGSize(width: collectionWidth/3, height: collectionWidth/3)
+//
+//    }
 }
